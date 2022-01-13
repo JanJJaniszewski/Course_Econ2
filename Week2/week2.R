7e0a416d0aa2af4cd0e8f1d99b9268428080f5d5
Jan Janiszewski <pole19901990@gmail.com>
2022-01-13 18:00:34 +0100

    Q2 got to iii but there is still an issue

--- Week2/week2.R
+++ Week2/week2.R
@@ -1,7 +1,37 @@
 library(pacman)
-p_load(haven)
+p_load(haven, SciViews, plm)
 
 #### 2 ####
 q2 <- read_dta("Week2/assignment2.dta")
 
 ##### i #####
+pooled_with <-
+  plm(
+    ln(earnings) ~ school + age + I(agesq ^ 2) + ethblack + urban + regne + regnc + regw + regs + asvabc,
+    data = q2, model = "pooling"
+  )
+pooled_without <-
+  plm(
+    ln(earnings) ~ school + age + I(agesq ^ 2) + ethblack + urban + regne + regnc + regw + regs,
+    data = q2, model = "pooling"
+  )
+
+pooled_with$coefficients['school']
+pooled_without$coefficients['school']
+
+##### ii #####
+pooled_ethnic <-
+  plm(
+    ln(earnings) ~ school + age + I(agesq ^ 2) + ethblack + urban + regne + regnc + regw + regs + I(school *
+                                                                                                      ethblack),
+    data = q2, model = "pooling"
+  )
+summary(pooled_ethnic)
+
+##### iii #####
+rem_ethnic <-
+  plm(
+    ln(earnings) ~ school + age + I(agesq ^ 2) + ethblack + urban + regne + regnc + regw + regs + I(school *
+                                                                                                      ethblack),
+    data = q2, model = "random", effect = "time"
+  )
