61cb7e7238e4420d2e98e197732688354ad737d0
Jan Janiszewski <pole19901990@gmail.com>
2022-01-19 15:10:33 +0100

    first model update

---
+++ Week3/Econ2Week3.R
@@ -0,0 +1,38 @@
+#### Load packages and init ####################################################
+if (!require("pacman")) install.packages("pacman")
+pacman::p_load(tidyverse, vtreat, caret, rpart, ohenery, xtable)
+set.seed(42)
+
+#### Data #################################################################
+##### Load #####
+q2 <- read_dta("Week3/assignment3.dta")
+q2 <- drop_na(q2) %>% select(-id)
+
+#### Q2 ####
+##### 2i #####
+# How many in group and how many would lie
+q2 %>% group_by(treatment) %>% count
+q2 %>% group_by(treatment, correct) %>% count
+
+# Fraction of students assumed to have lied
+round(((21/51)-(1/6)) / (5/6),2) # Candy
+round(((19/55)-(1/6)) / (5/6),2) # Fruit
+round(((37/93)-(1/6)) / (5/6),2) # Nothing
+
+# Balanced datasets
+q2 %>% group_by(treatment) %>% summarise_if(is.numeric, mean) %>%
+  xtable(., type='latex')
+
+##### 2ii #####
+lm1 <- lm(correct ~ treatment, data=q2)
+
+summary(lm1) %>% xtable(., type='latex')
+summary(lm1) 
+
+lm1 <- lm(correct ~ ., data=q2)
+
+summary(lm1) %>% xtable(., type='latex')
+summary(lm1) 
+
+
+##### 2iii #####
